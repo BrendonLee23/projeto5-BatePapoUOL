473e680f919d5a6bdb7fef538e47d183ba4ff216
Brendo Moreira <104690858+BrendonLee23@users.noreply.github.com>
2023-04-15 16:14:47 -0300

    JS iniciado

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,1 +1,34 @@
-axios.defaults.headers.common['Authorization'] = 'tZD8ryu6BwaQQL8MHbpwC9yF';
+const dados = axios.defaults.headers.common['Authorization'] = 'tZD8ryu6BwaQQL8MHbpwC9yF';
+
+let digitarNome = prompt("Digite seu nome")
+let nome = digitarNome;
+
+const nomeUsuario = { nome: nome };
+const statusUrl = 'https://mock-api.driven.com.br/api/vm/uol/status';
+
+// Função para enviar a requisição de status
+function enviarStatus() {
+    axios.post(statusUrl, nomeUsuario, { headers: dados })
+        .then(() => {
+            console.log('Status enviado com sucesso');
+        })
+        .catch((error) => {
+            console.log('Erro ao enviar status:', error);
+        });
+}
+
+// Enviar a primeira requisição de status imediatamente
+enviarStatus();
+
+// Enviar a requisição de status a cada 5 segundos
+setInterval(enviarStatus, 5000);
+
+function buscarMensagens() {
+    const url = "https://mock-api.driven.com.br/api/vm/uol/messages";
+    return axios.get(url)
+        .then(response => response.data)
+        .catch(error => {
+            console.error(error);
+            throw new Error("Erro ao buscar mensagens");
+        });
+    }
