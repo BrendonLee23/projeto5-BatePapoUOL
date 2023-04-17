78eb2a3af13c56c64ae7eb5b70241442be4c4123
Brendo Moreira <104690858+BrendonLee23@users.noreply.github.com>
2023-04-16 22:22:21 -0300

    projeto finalizado

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,34 +1,86 @@
 const dados = axios.defaults.headers.common['Authorization'] = 'tZD8ryu6BwaQQL8MHbpwC9yF';
 
-let digitarNome = prompt("Digite seu nome")
-let nome = digitarNome;
+var nome;
 
-const nomeUsuario = { nome: nome };
-const statusUrl = 'https://mock-api.driven.com.br/api/vm/uol/status';
+adicionarUsuario()
 
 // Função para enviar a requisição de status
 function enviarStatus() {
-    axios.post(statusUrl, nomeUsuario, { headers: dados })
-        .then(() => {
-            console.log('Status enviado com sucesso');
-        })
-        .catch((error) => {
-            console.log('Erro ao enviar status:', error);
-        });
+    let promessa = axios.post('https://mock-api.driven.com.br/api/vm/uol/status', { name: nome })
+    promessa.then(() => {
+        console.log("conectado com o servidor")
+    })
+    promessa.catch(() => {
+        console.log("desconectado do servidor")
+    })
 }
 
-// Enviar a primeira requisição de status imediatamente
-enviarStatus();
 
-// Enviar a requisição de status a cada 5 segundos
-setInterval(enviarStatus, 5000);
+// adicionar usuario e verificar se já existe
+function adicionarUsuario() {
+    let digitarNome = prompt("Digite seu nome")
+    nome = digitarNome;
+    let promessa = axios.post('https://mock-api.driven.com.br/api/vm/uol/participants', { name: nome })
+    promessa.then(() => {
+        criarMensagem()
+        setInterval(() => {
+            enviarStatus()
+        }, 5000);
+        setInterval(() => {
+            criarMensagem()
+        }, 3000);
+        console.log("Cadastro realizado com Sucesso!")
+    })
+    promessa.catch(() => {
+        window.alert("o nome de usuário ja está em uso")
+        window.location.reload()
+    })
+}
 
-function buscarMensagens() {
-    const url = "https://mock-api.driven.com.br/api/vm/uol/messages";
-    return axios.get(url)
-        .then(response => response.data)
+var listaDeMensagem = document.getElementById("saguão");
+const mensagemInput = document.querySelector("#mensagem");
+const enviarMensagem = document.querySelector("[data-test='send-message']");
+
+function criarMensagem() {
+    // executar um post na API
+    let promessa = axios.get('https://mock-api.driven.com.br/api/vm/uol/messages')
+    // adicionar todas as mensagem em "li"
+    
+    promessa.then((Promessa) => {
+        let mensagens = Promessa.data
+        listaDeMensagem.innerHTML = ""
+        for(let i = 0; i < mensagens.length; i++){
+            if (mensagens[i].type === "status"){
+                listaDeMensagem.innerHTML += `<li data-test="message"> <span class="span-time">(${mensagens[i].time})</span><span class="span-bold">${mensagens[i].from}</span> ${mensagens[i].text}</li>`
+            }
+            else if (mensagens[i].type === "message"){
+                listaDeMensagem.innerHTML += `<li data-test="message"><span class="span-time">(${mensagens[i].time})</span><span class="span-bold">${mensagens[i].from}</span> para <span class="span-bold">${mensagens[i].to}</span> ${mensagens[i].text}</li>`
+            }
+        }
+    })
+}
+
+// Adiciona um ouvinte de evento de clique no botão "Enviar mensagem"
+function enviarMSG() {
+    const message = mensagemInput.value;
+    
+    let objMensagem = {
+        from: nome,
+        to: "Todos",
+        text: mensagemInput.value,
+        type: "message"
+    }
+    console.log(objMensagem)
+    
+    // Envia a mensagem usando Axios
+    axios.post("https://mock-api.driven.com.br/api/vm/uol/messages", objMensagem) 
+    
+        .then(response => {
+            console.log(response)
+            document.querySelector("#mensagem").value = ""
+        })
         .catch(error => {
             console.error(error);
-            throw new Error("Erro ao buscar mensagens");
+            window.alert("Erro ao enviar mensagem");
         });
-    }
+}
